3f4c82fd432f1ae255d30c8d99199e56f0decab7
ProudNamwera4 <proudnamwera4@gmail.com>
2025-05-21 16:51:21 +0200

    w01 project: Contacts Part 1

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,19 @@
 const express = require("express");
+
+const mongodb = require("./data/database");
 const app = express();
-const port = process.env.Port || 8080;
+const port = process.env.PORT || 8080;
 
 // Middleware to parse JSON requests
 app.use("/", require("./routes"));
 
 // Start the server
-app.listen(port, () => {
-  console.log(`Server listening on port ${port}`);
+mongodb.initDb((err) => {
+  if (err) {
+    require("express");
+  } else {
+    app.listen(port, () => {
+      console.log(`Server listening on port ${port}`);
+    });
+  }
 });
