3fee86abdd133f1915a66bf89579c397599497b0
ProudNamwera4 <proudnamwera4@gmail.com>
2025-05-30 09:58:54 +0200

    Add the PUT, POST, and DELETE routes

--- server.js
+++ server.js
@@ -1,16 +1,17 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const mongodb = require("./data/database");
 const app = express();
 const port = process.env.PORT || 8080;
 
-// Middleware to parse JSON requests
+app.use(bodyParser.json());
 app.use("/", require("./routes"));
 
 // Start the server
 mongodb.initDb((err) => {
   if (err) {
-    require("express");
+    console.log(err);
   } else {
     app.listen(port, () => {
       console.log(`Server listening on port ${port}`);
