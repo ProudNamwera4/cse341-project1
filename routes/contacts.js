684e0c25693f3b4d056efa5715f72075457c901b
ProudNamwera4 <proudnamwera4@gmail.com>
2025-07-18 10:18:42 +0200

    cse341 project1

--- routes/contacts.js
+++ routes/contacts.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const validation = require("../middleware/validate");
 
 const contactsController = require("../controllers/contacts");
 
